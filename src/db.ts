d94c81cf2986bc7f0b81ad34f3aa9cfc6545da59
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2022-11-01 18:38:43 -0300

    .env

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,13 @@
 import { DataSource } from "typeorm";
 import {User} from "./entities/User";
+const dotenv = require('dotenv');
+dotenv.config()
 
 export const AppDataSource = new DataSource ({
     type: "mysql",
     host: "localhost",
     username: "root",
-    password: ".virtual!baron?!!pablo",
+    password: process.env.MYSQL_PASSW,
     port: 3306,
     database: "typeormdb",
     entities: [User],
